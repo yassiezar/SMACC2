8fbf1c9a3f464c4ecdf3e977153ff3563fff6493
Jaycee Lock <jaycee.lock@gmail.com>
2024-06-19 18:35:36 +0100

    Fixing copyright, formatting

--- smacc2_client_library/http_client/src/http_client/http_client.cpp
+++ smacc2_client_library/http_client/src/http_client/http_client.cpp
@@ -20,33 +20,39 @@
 
 #include <http_client/http_client.hpp>
 
-namespace cl_http {
-ClHttp::ClHttp(const std::string& server_name, const int& timeout)
-    : initialized_{false},
-      timeout_{timeout},
-      server_{server_name},
-      worker_guard_{boost::asio::make_work_guard(io_context_.get_executor())},
-      ssl_context_{boost::asio::ssl::context::tlsv12_client} {
+namespace cl_http
+{
+ClHttp::ClHttp(const std::string & server_name, const int & timeout)
+: initialized_{false},
+  timeout_{timeout},
+  server_{server_name},
+  worker_guard_{boost::asio::make_work_guard(io_context_.get_executor())},
+  ssl_context_{boost::asio::ssl::context::tlsv12_client}
+{
   ssl_context_.set_default_verify_paths();
   ssl_context_.set_verify_mode(boost::asio::ssl::verify_peer);
 }
 
-ClHttp::~ClHttp() {
+ClHttp::~ClHttp()
+{
   worker_guard_.reset();
   tcp_connection_runner_.join();
 }
 
-void ClHttp::onInitialize() {
-  if (!initialized_) {
+void ClHttp::onInitialize()
+{
+  if (!initialized_)
+  {
     tcp_connection_runner_ = std::thread{[&]() { io_context_.run(); }};
     this->initialized_ = true;
   }
 }
 
-void ClHttp::makeRequest(const kHttpRequestMethod http_method,
-                         const std::string& path) {
+void ClHttp::makeRequest(const kHttpRequestMethod http_method, const std::string & path)
+{
   auto path_used = path;
-  if (path[0] != '/') {
+  if (path[0] != '/')
+  {
     std::reverse(path_used.begin(), path_used.end());
     path_used += '/';
     std::reverse(path_used.begin(), path_used.end());
@@ -57,16 +63,20 @@ void ClHttp::makeRequest(const kHttpRequestMethod http_method,
   RCLCPP_INFO(this->getLogger(), "Path %s", path_used.c_str());
   RCLCPP_INFO(this->getLogger(), "Port %s", server_.getPort().c_str());
 
-  if (server_.isSSL()) {
-    std::make_shared<ssl_http_session>(boost::asio::make_strand(io_context_),
-                                       ssl_context_, callbackHandler)
-        ->run(server_.getServerName(), path_used,
-              static_cast<boost::beast::http::verb>(http_method), HTTP_VERSION);
-  } else {
-    std::make_shared<http_session>(boost::asio::make_strand(io_context_),
-                                   callbackHandler)
-        ->run(server_.getServerName(), path_used,
-              static_cast<boost::beast::http::verb>(http_method), HTTP_VERSION);
+  if (server_.isSSL())
+  {
+    std::make_shared<ssl_http_session>(
+      boost::asio::make_strand(io_context_), ssl_context_, callbackHandler)
+      ->run(
+        server_.getServerName(), path_used, static_cast<boost::beast::http::verb>(http_method),
+        HTTP_VERSION);
+  }
+  else
+  {
+    std::make_shared<http_session>(boost::asio::make_strand(io_context_), callbackHandler)
+      ->run(
+        server_.getServerName(), path_used, static_cast<boost::beast::http::verb>(http_method),
+        HTTP_VERSION);
   }
 }
 }  // namespace cl_http
